0db440ca5b28dbd80e09a07a506cc614927c372a
mabhishek042 <mabhishek0425@gmail.com>
2023-08-01 09:22:13 +0530

    Data_types of JavaScript
    Data_types of JavaScript

---
+++ 03_typeOf.js
@@ -0,0 +1,22 @@
+var bankName;
+console.log(bankName);
+bankName = "SBI Bank"
+console.log(bankName);
+console.log(typeof bankName);
+
+var marks = '90%'
+console.log(marks);
+console.log(typeof marks);
+
+var isWorking;
+console.log(isWorking);
+isWorking = true;
+console.log(isWorking);
+console.log(typeof isWorking);
+
+var totalCount= "Hundered and Seven";
+console.log(totalCount);
+console.log(typeof totalCount);
+totalCount = 107;
+console.log(totalCount);
+console.log(typeof totalCount);
