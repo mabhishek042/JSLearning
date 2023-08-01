63d4984211ab03667f4eff9163594d3208a35fff
mabhishek042 <mabhishek0425@gmail.com>
2023-08-01 08:24:30 +0530

    NotRequired

---
+++ 01_variableAssign.js
@@ -0,0 +1,24 @@
+var myFullname;
+console.log(myFullname);
+myFullname = "Abhishek More"; //variable initialization
+console.log(myFullname);
+myFullname = "Abhishek Ravindra More";
+console.log(myFullname);
+
+var myLove = "I Love only JS";
+console.log(myLove);
+
+var firstName = "Abhishek";
+console.log(firstName);
+var lastName = "More";
+console.log(lastName);
+var age = "26";
+console.log(age);
+var address = "Karvenagar, Pune";
+console.log(address);
+var pinCode = "444052";
+console.log(pinCode);
+var gender = "Male";
+console.log(gender);
+var isMarried = false;
+console.log(isMarried);
