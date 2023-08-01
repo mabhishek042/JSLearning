5fc068b3f0c5f494478585350d517726092a16b6
mabhishek042 <mabhishek0425@gmail.com>
2023-08-01 09:12:18 +0530

    VariableAssign
    VariableAssign

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,11 @@
+var location = "Pune";
+console.log(location);
+console.log(typeof location);  
+
+var pin = 411052;
+console.log(pin);
+console.log(typeof pin);
+
+var isMarried = false;
+console.log(isMarried);
+console.log(typeof isMarried);
